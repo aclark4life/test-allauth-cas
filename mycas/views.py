e83de6f6590c2304f1c29433110da4011264f11e
Alex Clark <aclark@aclark.net>
2022-09-29 16:18:45 -0400

    Update

--- mycas/views.py
+++ mycas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from allauth_cas.views import CASAdapter
+
+from .providers import MyCASProvider
+
+
+class MyCASAdapter(CASAdapter):
+    provider_id = MyCASProvider.id
+    url = 'https://mycas.mydomain.net'  # The CAS server url
+    version = 3  # Select the CAS protocol version used by the CAS server: 1, 2, 3…
